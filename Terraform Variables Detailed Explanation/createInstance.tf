bdeaebc5a5810132107e0b334f1747be664b8c74
Oleksii Sterlikov <Oleksii.Sterlikov@gmail.com>
2024-11-06 14:27:46 +0200

    modified: createInstance.tf, variables.tf

--- Terraform Variables Detailed Explanation/createInstance.tf
+++ Terraform Variables Detailed Explanation/createInstance.tf
@@ -1,11 +1,9 @@
 
 resource "aws_instance" "MyFirstInstnace" {
-  ami           = lookup(var.AMIS, var.AWS_REGION)
+  ami           = "ami-0745b7d4092315796"
   instance_type = "t2.micro"
 
   tags = {
     Name = "demoinstnce"
   }
-
-  security_groups = var.Security_Group
 }
